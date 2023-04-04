bc247942760c02cb8efd12a0ad5dc34b7bac180a
Taras Mykolaichuk <mtarasio@gmail.com>
2023-04-04 15:25:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header';
+import Herosection from './components/Herosection/Herosection';
+import Blog from './components/Blog/Blog';
+import './App.css';
+
+function Web() {
+  return (
+    <>
+      <Header />
+      <Herosection />
+      <Blog />
+    </>
+  );
+}
+
+export default Web;
---
+++ src/components/Herosection/Herosection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Herosection.scss';
+
+function Herosection() {
+  return (
+    <div className='section-main'>
+      <h2 className='section-main__title'>Full-width banner image</h2>
+    </div>
+  );
+}
+
+export default Herosection;
